4cd6f2f3185b06897da805b28f470e1375d22030
JustinM95 <justin.mitchell09@gmail.com>
2020-09-28 12:52:41 -0500

    Initial commit

---
+++ src/main/java/HealthyHabits.java
@@ -0,0 +1,9 @@
+import org.springframework.boot.SpringApplication;
+
+public class HealthyHabits {
+
+    public void main(String[] args) {
+        SpringApplication.run(HealthyHabits.class, args);
+    }
+
+}
